ab399e771d5fe305e42ae38c7a3b58c0235c999f
p3clifford80 <64710440+p3clifford80@users.noreply.github.com>
2020-11-24 08:49:50 -0800

    Add files via upload

---
+++ Magpie4.java
@@ -0,0 +1,130 @@
+
+public class Magpie4
+{
+
+    public String getGreeting()
+    {
+        return "Hello, let's talk.";
+    }
+
+    /**
+     * Gives a response to a user statement
+     *
+     * @param statement
+     * the user statement
+     * @return a response based on the rules given
+     */
+    public String getResponse(String statement)
+    {
+        String response = "";
+        if (findKeyword(statement, "no") >= 0)
+        {
+            response = "Why so negative?";
+        }
+        else if (( findKeyword(statement, "mother") >= 0) ||
+                 ( findKeyword(statement, "father") >= 0) ||
+                 ( findKeyword(statement, "brother") >= 0) ||
+                 ( findKeyword(statement, "sister") >= 0)     )
+        
+        {
+            response = "Tell me more about your family.";
+        }
+        else
+        {
+            response = getRandomResponse();
+        }
+        return response;
+    }
+
+    /**
+     * Search for one word in phrase. The search is not case
+     * sensitive. This method will check that the given goal
+     * is not a substring of a longer string (so, for
+     * example, "I know" does not contain "no").
+     *
+     * @param statement
+     * the string to search
+     * @param goal
+     * the string to search for
+     * @param startPos
+     * the character of the string to begin the
+     * search at
+     * @return the index of the first occurrence of goal in
+     * statement or -1 if it's not found
+     */
+    private int findKeyword(String statement, String goal, int startPos)
+    {
+        // trim() removes white space before and after the letters of a string
+        String phrase = statement.trim().toLowerCase();
+        goal = goal.toLowerCase();
+        int index = statement.indexOf(goal);
+
+        boolean front = false;
+        boolean back = false; 
+
+
+        while ( index > -1 ) 
+        {
+            // test if the character before is a letter
+            // ASCII value of 'a' = 97, value of 'z' is 122
+            // use compareTo 
+            index = statement.indexOf(goal, index+1 ); 
+            
+        }
+
+        return -1;
+    }
+
+    /**
+     * Search for one word in phrase. The search is not case
+     * sensitive. This method will check that the given goal
+     * is not a substring of a longer string (so, for
+     * example, "I know" does not contain "no"). The search
+     * begins at the beginning of the string.
+     *
+     * @param statement
+     * the string to search
+     * @param goal
+     * the string to search for
+     * @return the index of the first occurrence of goal in
+     * statement or -1 if it's not found
+     */
+    private int findKeyword(String statement, String goal)
+    {
+        return findKeyword(statement, goal, 0);
+    }
+
+    /**
+     * Pick a default response to use if nothing else fits.
+     *
+     * @return a non-committal string
+     */
+    private String getRandomResponse()
+    {
+        final int NUMBER_OF_RESPONSES = 4;
+        double r = Math.random();
+        int whichResponse = (int) (r * NUMBER_OF_RESPONSES);
+        String response = "";
+
+        if (whichResponse == 0)
+        {
+            response = "Interesting, tell me more.";
+        }
+        else if (whichResponse == 1)
+        {
+            response = "Hmmm.";
+        }
+        else if (whichResponse == 2)
+        {
+            response = "Do you really think so?";
+        }
+        else if (whichResponse == 3)
+        {
+            response = "You don't say.";
+        }
+
+        return response;
+    }
+
+}
+
